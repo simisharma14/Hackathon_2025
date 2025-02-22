6607de398149c54cd626463aa1a85a3d84925e79
jeslynguo <jeslyn.guoo@gmail.com>
2025-02-22 16:51:36 -0500

    hello

--- frontend/hackathon-dashboard/src/App.js
+++ frontend/hackathon-dashboard/src/App.js
@@ -10,9 +10,12 @@ import TopPerformers from "./TopPerformers";
 // import MacroOutlook from "./pages/MacroOutlook";
 // import StockProfile from "./pages/StockProfile";
 // import TopPerformers from "./pages/TopPerformers";
+import { GlobalProvider } from "./GlobalContext";
+
 
 function App() {
   return (
+    <GlobalProvider> {/* Wrap the entire Router inside GlobalProvider */}
     <Router>
         <div className="App">
           {/* Route Definitions */}
@@ -33,6 +36,7 @@ function App() {
           </Routes>
         </div>
     </Router>
+    </GlobalProvider>
   );
 }
 
