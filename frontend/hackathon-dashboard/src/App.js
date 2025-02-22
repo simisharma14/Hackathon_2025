a6f481c2c5f0b3cf3039d7a9f16e561e2fde2986
Verzino, Andrew Michael <averzino3@gatech.edu>
2025-02-22 18:13:07 -0500

    mvp

--- frontend/hackathon-dashboard/src/App.js
+++ frontend/hackathon-dashboard/src/App.js
@@ -1,6 +1,5 @@
-
 import React, { useEffect } from "react";
-import { BrowserRouter as Router, Route, Routes} from "react-router-dom"; 
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Dashboard from "./dashboard"; // Import the Dashboard component
 import LandingPage from "./LandingPage"; // Import the LandingPage component
@@ -9,11 +8,11 @@ import ETFPage from "./ETFPage"; // Make sure Home is imported
 import TopPerformers from "./TopPerformers";
 import ProfileViews from "./ProfileViews";
 import StockProfile from "./StockProfile";
+import MacroOutlook from "./MacroOutlook";
 // import MacroOutlook from "./pages/MacroOutlook";
 // import StockProfile from "./pages/StockProfile";
 // import TopPerformers from "./pages/TopPerformers";
 import { GlobalProvider } from "./GlobalContext";
-
 
 function App() {
   useEffect(() => {
@@ -21,26 +20,28 @@ function App() {
     window.history.replaceState(null, "", "/");
   }, []);
   return (
-    <GlobalProvider> {/* Wrap the entire Router inside GlobalProvider */}
-    <Router>
-      <div className="App">
-        {/* Route Definitions */}
-        <Routes>
-          {/* Landing Page Route */}
-          <Route path="/" element={<LandingPage />} />
-          {/* Home Page Route */}
-          <Route path="/home" element={<Home />} /> {/* Home page route */}
-          {/* Dashboard Page Route */}
-          <Route path="/dashboard" element={<Dashboard />} />
-          {/* Top Performers Page Route */}
-          <Route path="/TopPerformers" element={<TopPerformers />} />
-          <Route path="/ETFPage" element={<ETFPage />} />
-          <Route path="/ProfileViews" element={<ProfileViews />} />
-          <Route path="/stock/:symbol" element={<StockProfile />} />
+    <GlobalProvider>
+      {" "}
+      {/* Wrap the entire Router inside GlobalProvider */}
+      <Router>
+        <div className="App">
+          {/* Route Definitions */}
+          <Routes>
+            {/* Landing Page Route */}
+            <Route path="/" element={<LandingPage />} />
+            {/* Home Page Route */}
+            <Route path="/home" element={<Home />} /> {/* Home page route */}
+            {/* Dashboard Page Route */}
+            <Route path="/dashboard" element={<Dashboard />} />
+            {/* Top Performers Page Route */}
+            <Route path="/TopPerformers" element={<TopPerformers />} />
+            <Route path="/ETFPage" element={<ETFPage />} />
+            <Route path="/ProfileViews" element={<ProfileViews />} />
+            <Route path="/stock/:symbol" element={<StockProfile />} />
             <Route path="/MacroOutlook" element={<MacroOutlook />} />
-        </Routes>
-      </div>
-    </Router>
+          </Routes>
+        </div>
+      </Router>
     </GlobalProvider>
   );
 }
