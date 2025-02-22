96cbe3f8942eb833af43ac4af50708ec39582ce4
simisharma14 <ssharma659@gatech.edu>
2025-02-22 11:42:27 -0500

    Adding Landing Page + Jes Changes

--- frontend/hackathon-dashboard/src/App.js
+++ frontend/hackathon-dashboard/src/App.js
@@ -3,37 +3,38 @@ import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
 import Layout from "./components/Layout";
 import "./App.css";
 import Dashboard from "./dashboard"; // Import the Dashboard component
-import Home from "./pages/Home";
-import MacroOutlook from "./pages/MacroOutlook";
-import StockProfile from "./pages/StockProfile"
-import TopPerformers from "./pages/TopPerformers";
+import LandingPage from "./LandingPage"; // Import the LandingPage component
+// import Home from "./pages/Home";
+// import MacroOutlook from "./pages/MacroOutlook";
+// import StockProfile from "./pages/StockProfile";
+// import TopPerformers from "./pages/TopPerformers";
 
 function App() {
   return (
     <Router>
       <Layout>
-      <div className="App">
-        {/* Navigation Links */}
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/dashboard">View Dashboard</Link>
-            </li>
-          </ul>
-        </nav>
+        <div className="App">
+          {/* Navigation Links */}
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">LandingPage</Link>
+              </li>
+              <li>
+                <Link to="/dashboard">View Dashboard</Link>
+              </li>
+            </ul>
+          </nav>
 
-        {/* Route Definitions */}
-        <Routes>
-          {/* Home Page Route */}
-          <Route path="/" element={<h1>Hello Simi and Jeslyn!</h1>} />
+          {/* Route Definitions */}
+          <Routes>
+            {/* Landing Page Route */}
+            <Route path="/" element={<LandingPage />} />
 
-          {/* Dashboard Page Route */}
-          <Route path="/dashboard" element={<Dashboard />} />
-        </Routes>
-      </div>
+            {/* Dashboard Page Route */}
+            <Route path="/dashboard" element={<Dashboard />} />
+          </Routes>
+        </div>
       </Layout>
     </Router>
   );
