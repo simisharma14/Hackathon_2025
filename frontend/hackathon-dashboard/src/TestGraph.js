7992e4a6baa674a02839eb94000d8278f03b3482
jeslynguo <jeslyn.guoo@gmail.com>
2025-02-22 15:55:19 -0500

    Landing page update

--- frontend/hackathon-dashboard/src/TestGraph.js
+++
@@ -1,3 +0,0 @@
-import React, { useState, useEffect } from "react";
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from "recharts";
-import Papa from "papaparse";
